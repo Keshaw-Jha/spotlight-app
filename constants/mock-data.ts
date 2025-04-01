05ed2fd5585b04865f7f12e007e689e776670e75
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2025-04-01 07:40:49 +0530

    feed creation

---
+++ constants/mock-data.ts
@@ -0,0 +1,50 @@
+export const STORIES = [
+  {
+    id: "1",
+    username: "Alice",
+    avatar: "https://avatar.iran.liara.run/public/66",
+    hasStory: true,
+  },
+  {
+    id: "2",
+    username: "Bob",
+    avatar: "https://avatar.iran.liara.run/public/30",
+    hasStory: false,
+  },
+  {
+    id: "3",
+    username: "Charlie",
+    avatar: "https://avatar.iran.liara.run/public/35",
+    hasStory: true,
+  },
+  {
+    id: "4",
+    username: "David",
+    avatar: "https://avatar.iran.liara.run/public/43",
+    hasStory: true,
+  },
+  {
+    id: "5",
+    username: "Emma",
+    avatar: "https://avatar.iran.liara.run/public/93",
+    hasStory: true,
+  },
+  {
+    id: "6",
+    username: "Sophia",
+    avatar: "https://avatar.iran.liara.run/public/75",
+    hasStory: true,
+  },
+  {
+    id: "7",
+    username: "Liam",
+    avatar: "https://avatar.iran.liara.run/public/2",
+    hasStory: true,
+  },
+  {
+    id: "8",
+    username: "Olivia",
+    avatar: "https://avatar.iran.liara.run/public/98",
+    hasStory: false,
+  },
+];
